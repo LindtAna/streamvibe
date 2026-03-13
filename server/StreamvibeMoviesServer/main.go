8b74655bcfe231552697f4df4f9abe1fbad930ff
LindtAna <anastasiialindt@gmail.com>
2026-03-13 21:09:32 +0100

    create basic endpoint for testing Gin-Gonic(go run .)

--- server/StreamvibeMoviesServer/main.go
+++ server/StreamvibeMoviesServer/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	router := gin.Default()
 
+	router.GET("/hi", func(c *gin.Context) {
+		c.String(200, "hi, endpoint!")
+	})
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server", err)
+	}
 }
