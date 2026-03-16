809e54b3ea091b4029105799faf7f5b34da6b2dd
LindtAna <anastasiialindt@gmail.com>
2026-03-16 20:10:02 +0100

    AuthMiddleware implementiert, um Nutzer bei jedem API-Request zu autorisieren, GetAccessToken:Bearer-Validierung, robustere Header-Prüfung, ValidateToken:Signaturprüfung, Token-Gültigkeit, Fehlerhandling

---
+++ server/StreamvibeMoviesServer/middleware/auth_middleware.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/LindtAna/streamvibe/server/StreamvibeMoviesServer/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token, err := utils.GetAccessToken(c)
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			c.Abort()
+			return
+		}
+
+		if token == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "No token provided"})
+			c.Abort() //stop the middleware chain
+			return
+		}
+		claims, err := utils.ValidateToken(token)
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+		c.Set("userId", claims.UserId) //Saving user data
+		c.Set("role", claims.Role)
+
+		c.Next() //middleware is finished, execute the next middleware/handler
+	}
+}
--- server/StreamvibeMoviesServer/utils/token_util.go
+++ server/StreamvibeMoviesServer/utils/token_util.go
@@ -2,10 +2,13 @@ package utils
 
 import (
 	"context"
+	"errors"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/LindtAna/streamvibe/server/StreamvibeMoviesServer/database"
+	"github.com/gin-gonic/gin"
 	jwt "github.com/golang-jwt/jwt/v5"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
@@ -91,4 +94,55 @@ func UpdateAllTokes(userId, token, refreshToken string) (err error) {
 		return err
 	}
 	return nil
+}
+
+func GetAccessToken(c *gin.Context) (string, error) {
+	authHeader := c.Request.Header.Get("Authorization") //Authorization: Bearer <token>
+
+	if authHeader == "" {
+		return "", errors.New("Authorization header is required")
+	}
+	// tokenString := authHeader[len("Bearer"):]
+	tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+	if !strings.HasPrefix(authHeader, "Bearer ") {
+		return "", errors.New("Invalid authorization header format")
+	}
+
+	if tokenString == "" {
+		return "", errors.New("Bearer token is required")
+	}
+
+	return tokenString, nil
+}
+
+func ValidateToken(tokenString string) (*SignedDetails, error) {
+	claims := &SignedDetails{}
+	// Verifies the signature, parses the payload, writes data to claims
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		claims,
+		func(token *jwt.Token) (interface{}, error) {
+
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, errors.New("Invalid signing method")
+			}
+
+			return []byte(SECRET_KEY_TOKEN), nil
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	if time.Now().After(claims.ExpiresAt.Time) {
+		return nil, errors.New("Token has expired")
+	}
+
+	return claims, nil
+
 }
